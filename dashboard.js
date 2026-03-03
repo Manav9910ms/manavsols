37ef92e5950229c237731870b092fa731da52822
Manav9910ms <manav991077@gmail.com>
2026-03-03 15:37:09 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -5,22 +5,29 @@ import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase
 const userInfo = document.getElementById("userInfo");
 const logoutBtn = document.getElementById("logoutBtn");
 
-// Protect route
 onAuthStateChanged(auth, async (user) => {
+
   if (!user) {
     window.location.href = "login.html";
   } else {
+
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
       const data = docSnap.data();
-      userInfo.innerText = `Welcome, ${data.name}`;
+
+      userInfo.innerHTML = `
+        <div class="dashboard-card">
+          <h3>Welcome, ${data.name}</h3>
+          <p><strong>Email:</strong> ${data.email}</p>
+          <p><strong>Member Since:</strong> ${new Date(data.createdAt.seconds * 1000).toLocaleDateString()}</p>
+        </div>
+      `;
     }
   }
 });
 
-// Logout
 logoutBtn.addEventListener("click", async () => {
   await signOut(auth);
   window.location.href = "login.html";
