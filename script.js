d3df4e93aa59cbfc3792cf29dd40f25b613c2f9d
Manav9910ms <manav991077@gmail.com>
2026-03-04 16:23:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,25 +13,36 @@ menuBtn.addEventListener("click", () => {
     nav.classList.toggle("show");
 });
 
-document.getElementById("serviceForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+import { collection, addDoc, serverTimestamp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const mobile = document.getElementById("mobile").value;
-    const service = document.getElementById("service").value;
-    const budget = document.getElementById("budget").value;
-    const description = document.getElementById("description").value;
+import { db } from "./firebase.js";
 
-    alert("Request submitted successfully!");
+document.getElementById("serviceForm").addEventListener("submit", async (e) => {
 
-    console.log({
-        name,
-        email,
-        mobile,
-        service,
-        budget,
-        description
-    });
+e.preventDefault();
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const mobile = document.getElementById("mobile").value;
+const service = document.getElementById("service").value;
+const budget = document.getElementById("budget").value;
+const description = document.getElementById("description").value;
+
+const trackingId = "MS-" + Math.floor(10000 + Math.random() * 90000);
+
+await addDoc(collection(db, "service_requests"), {
+    trackingId,
+    name,
+    email,
+    mobile,
+    service,
+    budget,
+    description,
+    status: "Pending",
+    timestamp: serverTimestamp()
+});
+
+alert("Request submitted successfully!\nTracking ID: " + trackingId);
 
 });
