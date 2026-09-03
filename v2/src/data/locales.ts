4face74f60b8244fd9fcfa073ccc98d9bc5e5858
Manav9910ms <manav991077@gmail.com>
2026-09-03 13:07:58 +0530

    Create V2 locale route data

---
+++ v2/src/data/locales.ts
@@ -0,0 +1,10 @@
+export const locales = [
+  { code: "ru", name: "Русский", path: "/ru/", lang: "ru" },
+  { code: "de", name: "Deutsch", path: "/de/", lang: "de" },
+  { code: "fr", name: "Français", path: "/fr/", lang: "fr" },
+  { code: "es", name: "Español", path: "/es/", lang: "es" },
+  { code: "tr", name: "Türkçe", path: "/tr/", lang: "tr" },
+  { code: "vi", name: "Tiếng Việt", path: "/vi/", lang: "vi" },
+  { code: "id", name: "Bahasa Indonesia", path: "/id/", lang: "id" },
+  { code: "hi", name: "हिन्दी", path: "/hi/", lang: "hi" },
+] as const;
