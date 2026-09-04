92b758afe70cfd1b60d6729f5c567f57064bf4bb
Manav9910ms <manav991077@gmail.com>
2026-09-05 04:53:28 +0530

    Enable Vercel server routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://manavsols.com",
   trailingSlash: "always",
-  output: "static",
+  output: "server",
+  adapter: vercel(),
 });
